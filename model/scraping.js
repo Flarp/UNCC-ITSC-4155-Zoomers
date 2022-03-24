441a9b17de0268f4253fb97219aa8482291b31c4
JustCallMeJoe1 <jstric42@uncc.edu>
2022-03-23 22:40:44 -0400

    Gathered all information for the research news portion of the homepage

--- model/scraping.js
+++ model/scraping.js
@@ -20,13 +20,13 @@ exports.getResearchHeadlines = async function() {
         //Grabbing headers from the html body
         let headerNodeList = domResponse.window.document.querySelectorAll(".article-title > a");
         let anchorInformation = Array.from(headerNodeList);
-        const headerText = [];
+        //const headerText = [];
 
         //We are only needing 3, change i to retreive more headers... (Not using all data).
         for(let i = 0; i < 3; i++) {
             headerText.push(anchorInformation[i].textContent);
         }
-        console.log(headerText); 
+        //console.log(headerText); 
 
         //Grabbing Date Posted from the html body
         let dateNodeList = domResponse.window.document.querySelectorAll(".article-post-date > span");
@@ -37,11 +37,48 @@ exports.getResearchHeadlines = async function() {
         for(let i = 0; i < 3; i++) {
             dateText.push(dateSpanInformation[i].textContent);
         }
-        console.log(dateText);
+        //console.log(dateText);
 
+        //Grabbing Image source from the HTML body
+        let imageNodeList = domResponse.window.document.querySelectorAll(".article-image > img");
+        let imageSrcInformation = Array.from(imageNodeList);
+        const imageSrc = [];
 
-           
+        //We are only needing 3, change i to retrieve more srcs... (Not using all the data).
+        for(let i = 0; i < 3; i++) {
+            imageSrc.push(imageSrcInformation[i].getAttribute("src"));
+        }
+        //console.log(imageSrc);
 
+        //Grabbing article teaser (exceprt) from the HTML body
+        let teaserNodeList = domResponse.window.document.querySelectorAll(".article-teaser");
+        let teaserInformation = Array.from(teaserNodeList);
+        let teaserInfo = [];
+
+        //PROBLEM!: The /n character is being grabbed as well... maybe some sort of splice is needed...
+        //We are only needing 3, change i to retrieve more teasers... (Not using all the data).
+        for (let i = 0; i < 3; i++) {
+            teaserInfo.push(teaserInformation[i].textContent);
+        }
+        //console.log(teaserInfo);
+
+        //Grabbing article link (To UNCC domain) from the HTML body
+        let linkNodeList = domResponse.window.document.querySelectorAll(".article-title > a");
+        let linkInformation = Array.from(linkNodeList);
+        const relativeLinks = [];
+        const absoluteLinks = [];
+        
+        //We are only needing 3, change i to retrieve more teasers... (Not using all the data). This retreives relative link. Expand to absolute
+        for (let i = 0; i < 3; i++) {
+            relativeLinks.push(linkInformation[i].getAttribute("href"));
+        }
+        
+        //Convert relative path to aboslute path preappend https://inside.charlotte.edu/
+        relativeLinks.forEach(link => {
+            absoluteLinks.push("https://inside.charlotte.edu/" + link);
+        });
+
+        //console.log(absoluteLinks);
 
     } catch(error) {
         console.log("Error when scraping research headline information.")
