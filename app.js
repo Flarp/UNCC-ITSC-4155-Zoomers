cf26d3628a3e4566b52a893a3bf634f1b061f386
John Hall <johngradyh@gmail.com>
2022-04-15 14:12:15 -0400

    added fields for comments, favorites. fixed formatting issues in several files

--- app.js
+++ app.js
@@ -7,17 +7,17 @@
 
 const express = require("express")
 const hbs = require("express-handlebars")
-const mongoose = require('mongoose');
-const MongoStore = require("connect-mongo");
-const session = require("express-session");
-const flash = require("connect-flash");
+const mongoose = require("mongoose")
+const MongoStore = require("connect-mongo")
+const session = require("express-session")
+const flash = require("connect-flash")
 
 const SALT_ROUNDS = 10
 
 const { User } = require("./model/model.js")
 const mainRoutes = require("./routes/route_holder")
 const searchRoutes = require("./routes/searchRoutes")
-const userRoutes = require("./routes/userRoutes");
+const userRoutes = require("./routes/userRoutes")
 
 const instance = hbs.create({
   extname: ".hbs",
@@ -26,18 +26,20 @@ const instance = hbs.create({
 
 const app = express()
 
-
 app.engine("hbs", instance.engine)
 const host = "localhost"
 let port = 3000
 app.set("view engine", "hbs")
 app.set("views", "./views")
 
-mongoose.connect('mongodb://localhost:27017/researchmyprofessor').then(_ => {
-  app.listen(port, host, ()=> {
-    console.log("Server is running on port", port)
-  });
-}).catch(error => console.log(error.message));
+mongoose
+  .connect("mongodb://localhost:27017/researchmyprofessor")
+  .then((_) => {
+    app.listen(port, host, () => {
+      console.log("Server is running on port", port)
+    })
+  })
+  .catch((error) => console.log(error.message))
 
 //Mount Middleware
 app.use(express.static("public"))
@@ -46,26 +48,28 @@ app.use(
     extended: true,
   })
 )
-app.use(session({
-  secret: "4155-SoftwareDev",
-  resave: false,
-  saveUninitialized: false,
-  cookie: {maxAge: 60*60*1000}, //Lifetime of an hour. Mins * seconds * 1000 (This is 1 hour)
-  store: new MongoStore({mongoUrl: 'mongodb://localhost:27017/researchmyprofessor'}) //Sessions will now be connected to our mongoDB server and store that information along with other info. Collection by default is named sessions
-}));
+app.use(
+  session({
+    secret: "4155-SoftwareDev",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60 * 60 * 1000 }, //Lifetime of an hour. Mins * seconds * 1000 (This is 1 hour)
+    store: new MongoStore({
+      mongoUrl: "mongodb://localhost:27017/researchmyprofessor",
+    }), //Sessions will now be connected to our mongoDB server and store that information along with other info. Collection by default is named sessions
+  })
+)
 
 //Establish flash messages
-app.use(flash());
+app.use(flash())
 
 //Divide up different flash responses, store these into res local variables that will be used in the template engine
 app.use((req, res, next) => {
-
   //Store success and error messages into res object (locals)
-  res.locals.successMessages = req.flash("success");
-  res.locals.errorMessages = req.flash("error");
-  next();
-
-});
+  res.locals.successMessages = req.flash("success")
+  res.locals.errorMessages = req.flash("error")
+  next()
+})
 
 //Setup main routes of application, link to main routes
 app.use("/", mainRoutes)
@@ -74,6 +78,4 @@ app.use("/", mainRoutes)
 app.use("/search", searchRoutes)
 
 //Setup user routes of application, link to user routes
-app.use("/user", userRoutes);
-
-
+app.use("/user", userRoutes)
--- model/professor.js
+++ model/professor.js
@@ -1,28 +1,34 @@
 const { default: mongoose } = require("mongoose")
 
 const Class = new mongoose.Schema({
-    dept: String,
-    code: String,
-    name: String
-  })
-  
-  const Research = new mongoose.Schema({
-    title: String,
-    amount: String,
-    sponsor: String,
-    beginDate: String,
-    endDate: String,
-    fiscalYear: Number
-  })
-  
-  const ProfessorSchema = new mongoose.Schema({
-    professor: String,
-    department: String,
-    researchAreas: [String],
-    broadResearchAreas: [String],
-    homePage: [String],
-    classes: [Class],
-    research: [Research]
-  })
+  dept: String,
+  code: String,
+  name: String,
+})
 
-  module.exports = new mongoose.model('Professor', ProfessorSchema)
+const Research = new mongoose.Schema({
+  title: String,
+  amount: String,
+  sponsor: String,
+  beginDate: String,
+  endDate: String,
+  fiscalYear: Number,
+})
+
+const Review = new mongoose.Schema({
+  title: String,
+  reviewText: String,
+})
+
+const ProfessorSchema = new mongoose.Schema({
+  professor: String,
+  department: String,
+  researchAreas: [String],
+  broadResearchAreas: [String],
+  homePage: [String],
+  classes: [Class],
+  research: [Research],
+  reviews: [Review],
+})
+
+module.exports = new mongoose.model("Professor", ProfessorSchema)
