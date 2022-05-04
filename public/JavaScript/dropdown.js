1f3e6936d7cb52011398aae39680d57c0791d32f
Joe S <joexstrickland@gmail.com>
2022-05-03 23:54:55 -0400

    Fixed Footer. Fixed Drop down menu. Stylized search research area page. This is the land where the pharoh died.

--- public/JavaScript/dropdown.js
+++
@@ -1,7 +0,0 @@
-/* 
-    ITSC 4155 Group 3 - The Zoomers
-    Frontend script for clicking links selected in the search dropdown menu in the header.
-    Date: May 2nd, 2022
-*/
-
-/* Write this tomorrow */
